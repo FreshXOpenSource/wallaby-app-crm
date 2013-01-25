3dd9cd1bfbf6197c9e09157f82b9511d66bac8ad
Dominik Roessler <dominik@freshx.de>
2013-01-25 01:39:38 +0100

    description

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ wallaby-app-crm
 ===============
 
 Easy but powerful CRM based on wallaby.
+
+For more information on wallaby visit http://wlby.freshx.de
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='wallaby-app-crm',
-      version='0.1.35',
+      version='0.1.36',
       url='http://freshx.de/wallaby/apps/crm',
       author='FreshX GbR',
       author_email='wallaby@freshx.de',
